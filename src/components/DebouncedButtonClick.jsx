1c81feb9d05f7412a463fe673c9723c53f7e5beb
JatinBisht2308 <jatinbisht2308@gmail.com>
2025-02-09 12:47:12 +0530

    added the effective code withe clearing timeouts whenever needed

--- src/components/DebouncedButtonClick.jsx
+++ src/components/DebouncedButtonClick.jsx
@@ -1,27 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const DebouncedButtonClick = () => {
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
+
   const handleButtonClick = () => {
     console.log("Button clicked");
-    setX(x + 1);
-    setTimeout(() => {
-      hanldeYChange();
-    }, 1000);
+    setX((prevX) => prevX + 1);
   };
-  const hanldeYChange = () => {
-    setY(y + 1);
-  };
+
+  useEffect(() => {
+    // when the component mounts then i dont want the timeout to be hit
+    if(x===0) return;
+    
+    const debouncedClick = setTimeout(() => {
+      setY((prev) => setY(prev + 1));
+      console.log("called timeout",y+1);
+    },1000);
+     
+    return () => {
+      console.log("clearing",debouncedClick);
+      clearTimeout(debouncedClick);}
+  },[x]);
+
   return (
     <div>
       <h1>Limited button click using debouncing</h1>
       <div>
-        <button onClick={handleButtonClick}>FCUK ME</button>
+        <button onClick={handleButtonClick}>CLICK ME</button>
         <button
-          style={{
-            marginLeft: "15px",
-          }}
+          style={{ marginLeft: "15px" }}
           onClick={() => {
             setX(0);
             setY(0);
@@ -31,8 +39,8 @@ const DebouncedButtonClick = () => {
         </button>
       </div>
 
-      <p>{x}</p>
-      <p>{y}</p>
+      <p>X: {x}</p>
+      <p>Y: {y}</p>
     </div>
   );
 };
